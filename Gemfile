2a16eb28f7e2a8435c78949ca7638640ba31cc22
Jim Harvey <jimrharvey@gmail.com>
2012-06-30 15:44:33 -0700

    initialize arkham app

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+gem 'pg'
+gem 'jquery-rails'
+
+gem 'thin'
+gem 'nokogiri'
+
+group :development do
+  gem 'pry'
+end
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'bootstrap-sass'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+# To use ActiveModel has_secure_password
+gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arkham::Application.config.secret_token = '481e9bae6961f5f63083810fd30059e7be62fdc7ec55c3b30e85e73d9927df75f100cceda03c6e18069bc104d541da5bd82119b0396aea72afcbeb84277bb144'
