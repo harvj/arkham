ffa0a60bed3a0d739801082a5e7de505336c6dad
Jim Harvey <jimrharvey@gmail.com>
2012-07-03 12:01:06 -0700

    random encounters display via ajax

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -5,6 +5,11 @@ class EncountersController < ApplicationController
       @encounters = Encounter.for_location(params[:location]).where(expansion_id: params[:expansions])
       @encounter = @encounters[rand(@encounters.count)]
     end
+
+    respond_to do |f|
+      f.html
+      f.js { render 'result' }
+    end
   end
 
 end
